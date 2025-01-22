d4cc3fec633f73241cd0538e218fb80a00caa977
gsd123236 <gauravdhotre2009@gmail.com>
2025-01-22 10:37:20 +0530

    Add files via upload

---
+++ src/model/Mail.java
@@ -0,0 +1,22 @@
+package education.email.send.Entity;
+
+public class Mail {
+    private String subject;
+    private String message;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
